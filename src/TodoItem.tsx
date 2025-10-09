909446e200699878d23078771156bb56ede848c7
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-10-09 21:19:46 +0300

    initial

---
+++ src/TodoItem.tsx
@@ -0,0 +1,126 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Todo } from './types/Todo';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+type Props = {
+  todo: Todo;
+  onDelete: (todoId: number, errorMessage?: string) => void;
+  onStatusChange: (todo: Todo) => void;
+  onRename: (todo: Todo, newTitle: string) => Promise<void>;
+  isLoading: boolean;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  onDelete,
+  onStatusChange,
+  onRename,
+  isLoading,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(todo.title);
+  const editFieldRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditing && editFieldRef.current) {
+      editFieldRef.current.focus();
+    }
+  }, [isEditing]);
+
+  const handleStatusChange = () => {
+    onStatusChange(todo);
+  };
+
+  const handleDelete = () => {
+    onDelete(todo.id);
+  };
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+    setNewTitle(todo.title);
+  };
+
+  const handleSave = async () => {
+    const trimmedTitle = newTitle.trim();
+
+    if (trimmedTitle === '') {
+      onDelete(todo.id);
+      setIsEditing(false);
+
+      return;
+    }
+
+    if (trimmedTitle !== todo.title) {
+      await onRename(todo, trimmedTitle);
+    }
+
+    setIsEditing(false);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSave();
+    }
+
+    if (event.key === 'Escape') {
+      setIsEditing(false);
+      setNewTitle(todo.title);
+    }
+  };
+
+  return (
+    <div data-cy="Todo" className={`todo ${todo.completed ? 'completed' : ''}`}>
+      {!isEditing ? (
+        <>
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+              checked={todo.completed}
+              onChange={handleStatusChange}
+            />
+          </label>
+
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={handleDoubleClick}
+          >
+            {todo.title}
+          </span>
+
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={handleDelete}
+          >
+            ×
+          </button>
+        </>
+      ) : (
+        <input
+          data-cy="TodoTitleField"
+          type="text"
+          className="todo__title-field"
+          placeholder="Empty todo will be deleted"
+          value={newTitle}
+          onChange={e => setNewTitle(e.target.value)}
+          onBlur={handleSave}
+          onKeyUp={handleKeyDown}
+          ref={editFieldRef}
+        />
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={`modal overlay ${isLoading ? 'is-active' : ''}`}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
